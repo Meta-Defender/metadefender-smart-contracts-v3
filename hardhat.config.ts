dd1d88a16d1026227c6f8736116db140663046ef
Vincent Lu <vincentlu@HaleydeMacBook-Pro.local>
2022-10-17 22:53:14 +0800

    fixed spelling

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,6 +13,15 @@ const config: HardhatUserConfig = {
             },
         },
     },
+    networks: {
+        goerli: {
+            url: 'https://eth-goerli.g.alchemy.com/v2/Msvs6vDCEMxCE6Bz_O79v2senWL5e8qq',
+            accounts: [
+                '7e8444e3b47e706d2190801a32c568edeaeab2fe16e9dafdb774a6fdc5a211b8',
+            ],
+            allowUnlimitedContractSize: true,
+        },
+    },
 };
 
 export default config;
