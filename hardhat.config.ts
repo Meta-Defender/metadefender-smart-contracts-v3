259f173df18d2142a4160b3677f37e033ffa9252
Vincent Lu <vincentlu@M1_Mac.lan>
2023-03-03 20:19:14 +0800

    fixed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,13 +18,23 @@ const config: HardhatUserConfig = {
         hardhat: {
             allowUnlimitedContractSize: true,
         },
-        mandala: {
+        mandala_localhost: {
             url: 'http://127.0.0.1:8545',
             accounts: {
                 mnemonic:
                     'fox sight canyon orphan hotel grow hedgehog build bless august weather swarm',
                 path: "m/44'/60'/0'/0",
             },
+            allowUnlimitedContractSize: true,
+            chainId: 595,
+        },
+        mandala: {
+            url: 'https://eth-rpc-mandala.aca-staging.network',
+            accounts: [
+                // JUST FOR TESTING, DO NOT USE IN PRODUCTION
+                '7e8444e3b47e706d2190801a32c568edeaeab2fe16e9dafdb774a6fdc5a211b8',
+                '7093e4c110c56ec578ff6b3247d5975f1e5819397c42a745ef01bda903cebe61',
+            ],
             allowUnlimitedContractSize: true,
             chainId: 595,
         },
